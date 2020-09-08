640f3143a1a3daa87284b9586992d68526cc1683
Carlos Angeles <33170529+CyberLas@users.noreply.github.com>
2020-09-08 00:40:38 -0500

    Projecto Recipe plantilla

---
+++ web-recipes/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace web_recipes.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
